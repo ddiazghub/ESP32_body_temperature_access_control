c5d6fc65bf889f70e252fa535a9229f06be843a6
ddiazghub <dedemoya@uninorte.edu.co>
2021-04-20 17:49:27 -0500

    Did some progress

--- nodemcu_access_control.ino
+++ nodemcu_access_control.ino
@@ -2,15 +2,16 @@
 // #include "src/setup.h"
 
 // Include Libraries
-#include "Arduino.h"
+#include <Arduino.h>
 #include "src/Lib/Buzzer.h"
 #include "src/Lib/HCSR04.h"
 #include "src/Lib/LED.h"
-#include "Wire.h"
-#include "SPI.h"
+#include <Wire.h>
+#include <SPI.h>
 #include "src/Lib/Adafruit_SSD1306.h"
 #include "src/Lib/Adafruit_MLX90614.h"
 #include "src/Lib/Adafruit_GFX.h"
+#include <Servo.h>
 
 // Pin Definitions
 #define BUZZER_PIN_SIG	0
@@ -19,7 +20,6 @@
 #define LEDG_PIN_VIN	14
 #define LEDR_PIN_VIN	12
 #define OLED_RST	15
-#define TRIGGER 20
 
 // Global variables and defines
 #define SCREEN_WIDTH 128
@@ -29,6 +29,7 @@ double temp_body;           // Vars for storing ambient and body temperature
 double calibration = 2.36;  // Idk
 double threshold = 38.0;        // Temperature threshold for denying access
 int maxDistance = 10;       // Max distance for taking temperature
+byte trigger = 't';       // Trigger
 
 // object initialization
 Buzzer buzzer(BUZZER_PIN_SIG);
@@ -37,12 +38,12 @@ Led ledG(LEDG_PIN_VIN);
 Led ledR(LEDR_PIN_VIN);
 Adafruit_SSD1306 display(OLED_RST);
 Adafruit_MLX90614 mlx = Adafruit_MLX90614();
+Servo servo;
 
 // Setup.
 void setup() 
 {
     Serial.begin(9600);
-    pinMode(TRIGGER, INPUT);
 
     //Initialize I2C objects
     mlx.begin(); 
@@ -55,14 +56,10 @@ void setup()
 }
 
 // Main
-void loop() 
-{
+void loop() {
     // If trigger not received display room temperature continously.
-    if (!digitalRead(TRIGGER)) {
+    if (!Serial.available()) {
       temp_amb = mlx.readAmbientTempC();
-
-      Serial.print("Room Temp = ");
-      Serial.println(temp_amb);
 
       display.clearDisplay();
       
@@ -80,6 +77,11 @@ void loop()
       return;
     }
 
+    byte serialInput = Serial.read();
+    if (!serialInput == trigger) {
+      return;
+    };
+
     // Ask person to stand at 10 cm or less from sensor.
     display.clearDisplay();
     printToDisplay(10, 20, 1, " Posicionese a máximo ");
@@ -87,16 +89,23 @@ void loop()
 
 
     // Wait
-    printToDisplay(30,50, 1, "Esperando...");
+    printToDisplay(10,50, 1, "Esperando...");
     while(ultrasonic.distance() > maxDistance) {
-      Serial.println("Waiting...");
-    }
+
+    };
+
+    countdown(3, 45, 50, 1);
+    if (ultrasonic.distance() > maxDistance) {
+      display.clearDisplay();
+      printToDisplay(10, 20, 1, " No se tomó la temperatura correctamente ");
+      delay(2000);
+      return;
+    };
 
     
-    // Take temperature and display
+    // Take temperature and send to RFID system.
     temp_body = mlx.readObjectTempC();
-    Serial.print("Body temp = ");
-    Serial.println(temp_body);
+    Serial.print(temp_body);
 
     display.clearDisplay();
     printToDisplay(10, 0, 1, " Temperatura corporal: ");
@@ -109,8 +118,12 @@ void loop()
       buzzer.on();
 
       printToDisplay(0, 53, 1, "Su temperatura está muy alta");
+      delay(2000);
       return;
     }
+
+    ledG.on();
+    printToDisplay(25, 53, 1, "Bienvenido");
 }
 
 
@@ -140,4 +153,13 @@ void printTemperature(byte x, byte y, byte size, double temperature) {
   display.print(temperature);
   display.print((char)247);
   display.print("C");
+}
+
+// Delay while printing amount of seconds left at specified location and with specified size.
+void countdown(int seconds, byte x, byte y, byte size) {
+  while (seconds > 0) {
+    printToDisplay(x, y, size, "" + seconds);
+    delay(1000);
+    seconds--;
+  }
 }
